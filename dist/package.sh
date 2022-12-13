d662be16ca7c3bdfcb6782e598d1269aa62e4d2a
Richard Herman <geekyeggo@users.noreply.github.com>
2022-12-13 22:32:07 +0000

    Add sdPlugin

---
+++ dist/package.sh
@@ -0,0 +1,10 @@
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+pushd $SCRIPTPATH/..
+dotnet publish -c Release -o ./dist/com.geekyeggo.windowsdisplaysettings.sdPlugin -r win-x64 --sc -p:PublishTrimmed=true
+cp ./src/DisplaySettings/manifest.json ./dist/com.geekyeggo.windowsdisplaysettings.sdPlugin/manifest.json
+popd
+
+pushd $SCRIPTPATH
+./DistributionTool.exe -b -i ../dist/com.geekyeggo.windowsdisplaysettings.sdPlugin -o ../dist
+popd
