915dca297d0c51f93ae0d443d555be91563c123a
Richard Herman <geekyeggo@users.noreply.github.com>
2022-12-13 22:45:32 +0000

    Relocate files

--- src/Serialization/ResolutionConverter.cs
+++ src/Serialization/ResolutionConverter.cs
@@ -5,11 +5,16 @@ namespace DisplaySettings.Serialization
     using System.Text.Json.Serialization;
     using DisplaySettings.Services;
 
+    /// <summary>
+    /// Provides a <see cref="JsonConverter{T}"/> for <see cref="Resolution"/>.
+    /// </summary>
     internal class ResolutionConverter : JsonConverter<Resolution?>
     {
+        /// <inheritdoc/>
         public override Resolution? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             => Resolution.TryCreate(reader.GetString(), out var result) ? result : null;
 
+        /// <inheritdoc/>
         public override void Write(Utf8JsonWriter writer, Resolution? value, JsonSerializerOptions options)
         {
             if (value is null)
